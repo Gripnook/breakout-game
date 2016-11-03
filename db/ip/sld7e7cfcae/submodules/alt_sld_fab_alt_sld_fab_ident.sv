f9759a6632c059cc6d68deb209133ae39a698ab9
Gripnook <XxAndreixX@hotmail.com>
2016-11-03 16:32:52 -0400

    Lab 3, Part 4

--- db/ip/sld7e7cfcae/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld7e7cfcae/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 7a80799ba3cd89d66522
+//   DESIGN_HASH   : b2c92756ec0f6b07ddb2
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h7a80799ba3cd89d66522 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb2c92756ec0f6b07ddb2 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
